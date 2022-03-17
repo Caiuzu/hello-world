93a68ba19add9ae0ce11cb446beaf7b427e3cce5
Caio Souza <caio.7s@outlook.com>
2022-03-17 12:31:34 -0300

    init: Inicializando projeto

---
+++ src/main/java/br/com/hello/world/HelloWorldApplication.java
@@ -0,0 +1,13 @@
+package br.com.hello.world;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/hello/world/HelloWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.hello.world;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
